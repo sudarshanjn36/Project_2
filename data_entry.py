b7d6c060c5ba68c7abd2703cc14c899e9aaf23c0
Sudarshan <sudarshanjn36@gmail.com>
2025-01-06 21:02:29 -0800

    first commi

---
+++ data_entry.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+def get_date(prompt,allow_default=False):
+    date_str = input(prompt)
+    date_format='%d-%m-%Y'
+    if allow_default and not date_str:
+        return datetime.today().strftime(date_format)  #strftime is string format time
+    
+    try:
+        valid_date = datetime.strftime(date_str,date_format)
+        return valid_date.strftime(date_format)
+    except ValueError:
+        print('Please enter valid date in dd-mm-yyyy format')
+        return get_date(prompt,allow_default)
+
+def amount():
+    pass
